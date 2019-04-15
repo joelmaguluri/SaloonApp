350a654c21d537f36306fa9dfa9b1514940f5803
sudeep987 <msudeep.joel@gmail.com>
2019-04-15 23:58:34 +0530

    signup-login

--- index.js
+++ index.js
@@ -6,20 +6,35 @@ const app = express();
 const port = process.env.PORT || 1234;
 const path=require('path')
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'));
 app.get('/',function(req,res,next){
 res.sendFile(path.join(__dirname,'/public/index.html'));
 
 });
-app.post('/',function(req,res,next){
+
+app.post('/login',function(req,res,next){
+
+    res.sendFile(path.join(__dirname,'/public/afterlogin.html'));
+
+})
+app.set('view engine', 'pug');
+app.post('/signup',function(req,res,next){
+   console.log(req.body)
     var user = new User();
     user.name=req.body.name;
     user.email=req.body.email;
     user.password=req.body.password;
     user.save();
-    res.json({"status":"success"});
+    res.render('thankyou.pug');
 
-});
+})
+
+app.get('/admin',function(req,res,next){
+    res.sendFile(path.join(__dirname,'/public/admin.html'));
+    
+    });
+
 app.listen(port,function(){
     console.log(`Server connected at ${port}`);
 });
